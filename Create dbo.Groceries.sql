c5139a6a57152d3973815a7f77b1a576631ae90b
koufiche <smounaj@gmail.com>
2019-02-26 21:39:49 -0500

    inClassSubmission

---
+++ Create dbo.Groceries.sql
@@ -0,0 +1,21 @@
+﻿USE [InClassIssignment]
+GO
+
+/****** Object: Table [dbo].[Groceries] Script Date: 2/26/2019 9:30:18 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Groceries] (
+    [GroceryID] INT        NOT NULL,
+    [Vegies]    NCHAR (10) NULL,
+    [Fruit]     NCHAR (10) NULL,
+    [Dairy]     NCHAR (10) NULL,
+    [Meat]      NCHAR (10) NULL,
+    [Bakery]    NCHAR (10) NULL,
+    [Pharmacy]  NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([GroceryID] ASC)
+);
+
---
+++ Create dbo.Sales.sql
@@ -0,0 +1,20 @@
+﻿USE [InClassIssignment]
+GO
+
+/****** Object: Table [dbo].[Sales] Script Date: 2/26/2019 9:31:44 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sales] (
+    [SalesId]       INT           NOT NULL,
+    [SalesItem]     NVARCHAR (50) NULL,
+    [SalesCategory] NVARCHAR (50) NULL,
+    [GroceryID]     INT           NULL,
+    PRIMARY KEY CLUSTERED ([SalesId] ASC),
+    FOREIGN KEY ([GroceryID]) REFERENCES [dbo].[Groceries] ([GroceryID])
+);
+
+
